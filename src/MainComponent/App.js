fa13ce00cc303bfa23598f5cf856b6dbafdcc32c
Azzi-Mo <alhwarymostafa@gmail.com>
2022-11-30 22:45:07 +0200

    updatefrom lab

--- src/MainComponent/App.js
+++ src/MainComponent/App.js
@@ -1,15 +1,11 @@
-import './App.scss'
-import MainPage from '../Pages/Mainpage'
-function App()
-{
-    return (
-        <>
-           <section className="container sec_cont" style={{'height': window.innerHeight }}>
-
-              <MainPage/>
-
-           </section>
-        </>
-    )
+// import MainPage from "../Pages/Mainpage";
+import Products from "./Pages/Product";
+import { useState } from "react";
+function App() {
+  return(
+    <>
+    <Products cont=/>
+    </>
+  )
 }
-export default App
+export default App;
